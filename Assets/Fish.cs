c41e1ed62864922573b628fe1e0cb2fee203a1d8
Angel Lin <angel.tian.lin@gmail.com>
2013-09-24 12:28:37 -0400

    Aquarium proto

---
+++ Assets/Fish.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fish : MonoBehaviour {
+	public Vector3 destination = new Vector3 (5f, 5f, 0f);
+	public float speed = 5f;
+		
+	// Use this for initialization
+	
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//makes it face the direction its swimming in
+		
+		transform.rotation = Quaternion.LookRotation (rigidbody.velocity);
+	
+	}
+	
+	//Update takes place in unguarenteed time between frames. Fixed Update will update on a fixed interval (ex. every 0.02 games)
+	void FixedUpdate() {
+		//makes fish swim forward
+		Vector3 direction = Vector3.Normalize (destination - transform.position);
+		
+		//awkward zooming of fish	
+		//rigidbody.AddForce( destination - transform.position );
+		//rigidbody.AddForce(destination)
+		rigidbody.AddForce (direction *speed, ForceMode.VelocityChange);
+}
+} 
---
+++ Assets/FishGod.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishGod : MonoBehaviour {
+	public Fish fishBlueprint;
+	public int fishCount = 100;
+	
+	
+	// Use this for initialization
+	void Start () {
+		int currentFishcounter = 0;
+		while (currentFishcounter < fishCount){
+			//Spawn a fish
+			Vector3 fishPosition = new Vector3(Random.Range (-10f,10f), Random.Range(-10f, 10f), Random.Range (-10f, 10f) );
+			
+			//will instantiate blueprint, at this position, with no rotation
+			Instantiate (fishBlueprint, fishPosition, Quaternion.identity);
+			
+			//increment fishcounter
+			currentFishcounter ++;
+		}
+	
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
